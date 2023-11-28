21763910f3e22c0f2325a9d76405f498617c0aa7
Lucio <luciooliver64@gmail.com>
2023-11-27 23:04:32 -0300

    Atualização dos scripts de banco para criar as tabelas.

--- script.sql
+++ script.sql
@@ -1,10 +1,16 @@
 CREATE TABLE IF NOT EXISTS usuario_cliente(
 	id SERIAL PRIMARY KEY,
 	cpf TEXT,
-	nome text,
-	email text,
-	senha text
+	nome TEXT,
+	email TEXT,
+	senha TEXT
 );
+
+ALTER TABLE usuario_cliente
+ADD CONSTRAINT cpf_unique UNIQUE (cpf);
+
+ALTER TABLE usuario_cliente
+ADD CONSTRAINT email_unique UNIQUE (email);
 
 CREATE TABLE IF NOT EXISTS hotel(
 	id SERIAL PRIMARY KEY,
@@ -20,17 +26,16 @@ CREATE TABLE IF NOT EXISTS quarto (
     hotel_id INT REFERENCES hotel(id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS disponibilidade_quarto (
-    id SERIAL PRIMARY KEY,
-    quarto_id INT REFERENCES quarto(id) ON DELETE CASCADE,
-    data_inicio DATE NOT NULL,
-    data_fim DATE NOT null,
-    reservado BOOLEAN DEFAULT FALSE
-);
+ALTER TABLE quarto
+ADD COLUMN localizacao TEXT;
 
 CREATE TABLE IF NOT EXISTS reserva (
     id SERIAL PRIMARY KEY,
     data_inicio DATE NOT NULL,
     data_fim DATE NOT NULL,
-    quarto_id INT REFERENCES quarto(id) ON DELETE cascade
+    quarto_id INT REFERENCES quarto(id) ON DELETE cascade,
+    usuario_cliente_id INT REFERENCES usuario_cliente(id) ON DELETE cascade
 );
+
+ALTER TABLE reserva
+ADD COLUMN forma_pagamento INT;
